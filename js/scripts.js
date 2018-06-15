2937a5212a2642c317c51b3ce7cebc7284d9d500
Igor A <autin.igor@gmail.com>
2018-06-15 10:49:44 -0600

    Sidebar Working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("alo alo alo");
+$('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+});
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});

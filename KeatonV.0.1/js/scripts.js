7f85306129250c0139554739e0dac2ed481cf523
Igor A <autin.igor@gmail.com>
2018-07-28 17:23:05 -0600

    Restarting Fresh

---
+++ KeatonV.0.1/js/scripts.js
@@ -0,0 +1,14 @@
+console.log("alo alo alo");
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
